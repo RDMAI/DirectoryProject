5fb7bbc2df88071bab013f3302761bfba4117563
RDMAI <aleksejgusakov09@gmail.com>
2025-06-11 17:13:53 +0300

    Added SoftDeleteCleanerBackgroundService with configurations in appsettings.json

--- src/DirectoryService/DirectoryProject.DirectoryService.Infrastructure/BackgroundServices/SoftDeleteCleanerBackgroundService.cs
+++ src/DirectoryService/DirectoryProject.DirectoryService.Infrastructure/BackgroundServices/SoftDeleteCleanerBackgroundService.cs
@@ -1,121 +1,88 @@
-﻿//using Dapper;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Hosting;
-//using Microsoft.Extensions.Logging;
-//using Microsoft.Extensions.Options;
+﻿using Dapper;
+using DirectoryProject.DirectoryService.Application.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-//namespace DirectoryProject.DirectoryService.Infrastructure.BackgroundServices;
+namespace DirectoryProject.DirectoryService.Infrastructure.BackgroundServices;
 
-//public class SoftDeleteCleanerBackgroundService : BackgroundService
-//{
-//    private readonly IServiceScope _serviceScope;
-//    private readonly ILogger<SoftDeleteCleanerBackgroundService> _logger;
-//    private readonly IDBConnectionFactory _dBConnectionFactory;
+public class SoftDeleteCleanerBackgroundService : BackgroundService
+{
+    private readonly IDBConnectionFactory _connectionFactory;
+    private readonly ILogger<SoftDeleteCleanerBackgroundService> _logger;
 
-//    private readonly TimeSpan _checkPeriod;
-//    private readonly TimeSpan _timeToRestore;
+    private readonly TimeSpan _checkPeriod;
+    private readonly TimeSpan _timeToRestore;
 
-//    public SoftDeleteCleanerBackgroundService(
-//        IDbContextFactory<PetsWriteDBContext> dbFactory,
-//        ILogger<SoftDeleteCleanerBackgroundService> logger,
-//        [FromKeyedServices(DependencyKey.Pets)] IDBConnectionFactory dBConnectionFactory,
-//        IMessageQueue<IEnumerable<FileInfoDTO>> fileMessageQueue,
-//        IOptions<SoftDeleteCleanerOptions> options)
-//    {
-//        _dbFactory = dbFactory;
-//        _logger = logger;
-//        _dBConnectionFactory = dBConnectionFactory;
-//        _fileMessageQueue = fileMessageQueue;
-//        if (options != null)
-//        {
-//            _checkPeriod = TimeSpan.FromHours(options.Value.CheckPeriodHours);
-//            _timeToRestore = TimeSpan.FromHours(options.Value.TimeToRestoreHours);
-//        }
-//    }
+    public SoftDeleteCleanerBackgroundService(
+        IDBConnectionFactory connectionFactory,
+        ILogger<SoftDeleteCleanerBackgroundService> logger,
+        IOptions<SoftDeleteCleanerOptions> options)
+    {
+        _connectionFactory = connectionFactory;
+        _logger = logger;
 
-//    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-//    {
-//        _logger.LogInformation("SoftDeleteCleanerBackgroundService started");
+        if (options != null)
+        {
+            _checkPeriod = TimeSpan.FromHours(options.Value.CheckPeriodHours);
+            _timeToRestore = TimeSpan.FromHours(options.Value.TimeToRestoreHours);
+        }
+    }
 
-//        using PeriodicTimer timer = new(_checkPeriod);
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("SoftDeleteCleanerBackgroundService started");
 
-//        // when stop requested, waiting for timer to tick will be cancelled too
-//        while (!stoppingToken.IsCancellationRequested &&
-//            await timer.WaitForNextTickAsync(stoppingToken))
-//        {
-//            try
-//            {
-//                using var context = _dbFactory.CreateDbContext();
+        using PeriodicTimer timer = new(_checkPeriod);
 
-//                // deleting pets
-//                await _deletePets(context, stoppingToken);
+        // when stop requested, waiting for timer to tick will be cancelled too
+        while (!stoppingToken.IsCancellationRequested &&
+            await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            try
+            {
+                using var connection = _connectionFactory.Create();
 
-//                // deleting volunteers
-//                var deletedVolunteers = context.Volunteers
-//                    .Where(v => v.IsDeleted &&
-//                        DateTime.UtcNow - v.DeletionDate!.Value >= _timeToRestore);
+                var departmentDeleteResult = await connection.ExecuteAsync(new CommandDefinition(
+                    $"""
+                    DELETE FROM diretory_service.departments
+                    WHERE is_active = false AND
+                        '{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}' - updated_at >= '{_timeToRestore}'
+                    """,
+                    cancellationToken: stoppingToken));
 
-//                // this will also cascade delete related pets
-//                context.Volunteers.RemoveRange(deletedVolunteers);
-//                await context.SaveChangesAsync(stoppingToken);
+                var locationDeleteResult = await connection.ExecuteAsync(new CommandDefinition(
+                    $"""
+                    DELETE FROM diretory_service.locations
+                    WHERE is_active = false AND
+                        '{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}' - updated_at >= '{_timeToRestore}'
+                    """,
+                    cancellationToken: stoppingToken));
 
-//                _logger.LogInformation("SoftDeleteCleanerBackgroundService executed successfully");
-//            }
-//            catch (Exception ex)
-//            {
-//                _logger.LogError(ex, ex.Message);
-//            }
-//        }
+                var positionDeleteResult = await connection.ExecuteAsync(new CommandDefinition(
+                    $"""
+                    DELETE FROM diretory_service.positions
+                    WHERE is_active = false AND
+                        '{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}' - updated_at >= '{_timeToRestore}'
+                    """,
+                    cancellationToken: stoppingToken));
 
-//        _logger.LogInformation("SoftDeleteCleanerBackgroundService finished");
-//    }
+                _logger.LogInformation("SoftDeleteCleanerBackgroundService executed successfully");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+        }
 
-//    private class _petPhotosDTO
-//    {
-//        public Guid Id { get; init; }
-//        public FileVO[] Photos { get; init; } = [];
-//    }
+        _logger.LogInformation("SoftDeleteCleanerBackgroundService finished");
+    }
 
-//    private async Task _deletePets(PetsWriteDBContext context, CancellationToken stoppingToken)
-//    {
-//        using var connection = _dBConnectionFactory.Create();
-
-//        // Getting all required to delete pets. Retrieving id and file paths
-//        var petsSql = $"""
-//            SELECT id, photos
-//            FROM pets_management.Pets
-//            WHERE is_deleted AND
-//                '{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}' - deletion_date >= '{_timeToRestore}'
-//            """;
-
-//        _logger.LogInformation("Dapper pets select sql:\n{sql}", petsSql);
-//        var pets = await connection.QueryAsync<_petPhotosDTO>(petsSql);
-//        if (pets is null || pets.Any() == false)
-//            return;
-
-//        // Creating a list of ids for postgres
-//        var deletedPetIds = "('" + string.Join("', '", pets.Select(p => p.Id)) + "')";
-
-//        var petsDeleteSQL = $"""
-//            DELETE FROM pets_management.Pets
-//            WHERE id IN {deletedPetIds}
-//            """;
-//        _logger.LogInformation("Pets delete sql:\n{sql}", petsSql);
-
-//        // With raw sql it is one database call. With DDD, it will be 2 (get volunteers and update volunteers)
-//        var deleteResult = await context.Database.ExecuteSqlRawAsync(petsDeleteSQL, stoppingToken);
-
-//        // Placing an operation to delete photos of deleted pets
-//        List<FileInfoDTO> fileInfos = [];
-//        foreach (var p in pets)
-//        {
-//            var f = p.Photos.Select(photo => new FileInfoDTO(
-//                photo.PathToStorage,
-//                Constants.BucketNames.PET_PHOTOS));
-//            fileInfos.AddRange(f);
-//        }
-//        if (fileInfos.Count != 0)
-//            await _fileMessageQueue.WriteAsync(fileInfos, stoppingToken);
-//    }
-//}
+    public class SoftDeleteCleanerOptions
+    {
+        public required float CheckPeriodHours { get; set; }
+        public required float TimeToRestoreHours { get; set; }
+    }
+}
--- src/DirectoryService/DirectoryProject.DirectoryService.Infrastructure/DependencyInjection.cs
+++ src/DirectoryService/DirectoryProject.DirectoryService.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,13 @@
 ﻿using DirectoryProject.DirectoryService.Application.Interfaces;
 using DirectoryProject.DirectoryService.Application.Shared.Interfaces;
+using DirectoryProject.DirectoryService.Infrastructure.BackgroundServices;
 using DirectoryProject.DirectoryService.Infrastructure.DatabaseRead;
 using DirectoryProject.DirectoryService.Infrastructure.DatabaseWrite;
 using DirectoryProject.DirectoryService.Infrastructure.DatabaseWrite.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using static DirectoryProject.DirectoryService.Infrastructure.BackgroundServices.SoftDeleteCleanerBackgroundService;
 
 namespace DirectoryProject.DirectoryService.Infrastructure;
 
@@ -20,13 +22,18 @@ public static class DependencyInjection
         var dbConnectionString = configuration.GetConnectionString(ApplicationWriteDBContext.DATABASE_CONFIGURATION);
         services.AddScoped(_ => new ApplicationWriteDBContext(dbConnectionString));
 
-        services.AddScoped<IDBConnectionFactory>(_ => new ReadDBConnectionFactory(dbConnectionString));
+        services.AddSingleton<IDBConnectionFactory>(_ => new ReadDBConnectionFactory(dbConnectionString));
 
         services.AddScoped<ILocationRepository, LocationRepository>();
         services.AddScoped<IDepartmentRepository, DepartmentRepository>();
         services.AddScoped<IPositionRepository, PositionRepository>();
 
         services.AddScoped<IUnitOfWork, DirectoryServiceUnitOfWork>();
+
+        var configs = configuration.GetSection("SoftDeleteCleaner");
+
+        services.Configure<SoftDeleteCleanerOptions>(configs);
+        services.AddHostedService<SoftDeleteCleanerBackgroundService>();
 
         return services;
     }
