df2a74578d7becb8aa0fc10f7d44e23779365594
RDMAI <aleksejgusakov09@gmail.com>
2025-06-29 13:46:23 +0300

    Remade DepartmentTreeDTO to record

--- src/DirectoryService/DirectoryProject.DirectoryService.Application/DTOs/DepartmentTreeDTO.cs
+++ src/DirectoryService/DirectoryProject.DirectoryService.Application/DTOs/DepartmentTreeDTO.cs
@@ -2,28 +2,22 @@
 
 namespace DirectoryProject.DirectoryService.Application.DTOs;
 
-public class DepartmentTreeDTO
+public record DepartmentTreeDTO(
+    Guid Id,
+    string Name,
+    string Path,
+    short Depth,
+    Guid? ParentId,
+    int ChildrenCount,
+    List<DepartmentTreeDTO> Children)
 {
-    public Guid Id { get; private set; }
-    public string Name { get; private set; }
-    public string Path { get; private set; }
-    public short Depth { get; private set; }
-    public Guid? ParentId { get; private set; }
-    public List<DepartmentTreeDTO> Children { get; private set; } = [];
-    public int ChildrenCount { get; private set; }
-
-    public DepartmentTreeDTO()
-    {
-    }
-
     public static DepartmentTreeDTO FromDomainEntity(Department entity, IEnumerable<Department>? children = null)
-        => new DepartmentTreeDTO
-        {
-            Id = entity.Id.Value,
-            Name = entity.Name.Value,
-            Path = entity.Path,
-            Depth = entity.Depth,
-            ChildrenCount = entity.ChildrenCount,
-            Children = children is null ? [] : children.Select(d => FromDomainEntity(d)).ToList(),
-        };
+        => new DepartmentTreeDTO(
+            Id: entity.Id.Value,
+            Name: entity.Name.Value,
+            Path: entity.Path,
+            Depth: entity.Depth,
+            ParentId: entity.ParentId?.Value,
+            ChildrenCount: entity.ChildrenCount,
+            Children: children is null ? [] : children.Select(d => FromDomainEntity(d)).ToList());
 }
